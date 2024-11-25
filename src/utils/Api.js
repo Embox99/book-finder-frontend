98ab6eca366330d1cf48abf140cab8aaea77529d
Eduard Vilensky <embox53@gmail.com>
2024-11-25 15:39:50 +0200

    working on requests to backend

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,8 @@
-const checServerResponce = (res) => {
+const BASE_URL = "production"
+  ? "https://api.book-finder.crabdance.com/"
+  : "http://localhost:3001";
+
+const checkServerResponce = (res) => {
   if (res.ok) {
     return res.json();
   } else {
@@ -6,4 +10,105 @@ const checServerResponce = (res) => {
   }
 };
 
-export { checServerResponce };
+function getCurrentUser(token) {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkServerResponce);
+}
+
+function updateCurrentUser(data, token) {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      name: data.name,
+      email: data.email,
+    }),
+  }).then(checkServerResponce);
+}
+
+function addFavoriteBook(data, token) {
+  return fetch(`${BASE_URL}/books/favorite`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  }).then(checkServerResponce);
+}
+
+function addReadBook(data, token) {
+  return fetch(`${BASE_URL}/books/read`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  }).then(checkServerResponce);
+}
+
+function removeFavoriteBook(id, token) {
+  return fetch(`${BASE_URL}/books/favorite/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkServerResponce);
+}
+
+function removeReadBook(id, token) {
+  return fetch(`${BASE_URL}/books/read/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkServerResponce);
+}
+
+function getUserFavoriteBooks(token) {
+  return fetch(`${BASE_URL}/books/favorite`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkServerResponce);
+}
+
+function getUserReadBooks(token) {
+  return fetch(`${BASE_URL}/books/read`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkServerResponce);
+}
+
+export {
+  checkServerResponce,
+  updateCurrentUser,
+  getCurrentUser,
+  addFavoriteBook,
+  addReadBook,
+  removeFavoriteBook,
+  removeReadBook,
+  getUserFavoriteBooks,
+  getUserReadBooks,
+};
--- src/utils/Books.js
+++ src/utils/Books.js
@@ -1,4 +1,4 @@
-import { checServerResponce } from "./Api";
+import { checkServerResponce } from "./Api";
 
 const API_KEY = "AIzaSyDS87FTNMkojvpmlUKiA8qLbIm8NrYqGhQ";
 
@@ -6,7 +6,7 @@ export const searchBooks = async (query) => {
   return fetch(
     `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`
   )
-    .then(checServerResponce)
+    .then(checkServerResponce)
     .then((data) => data.items)
     .catch((error) => {
       console.error("Error executing the request:", error);
@@ -18,7 +18,7 @@ export const getPopularBooks = async () => {
   return fetch(
     `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&orderBy=relevance&key=${API_KEY}`
   )
-    .then(checServerResponce)
+    .then(checkServerResponce)
     .then((data) => {
       if (data.items) {
         return data.items.sort(() => 0.5 - Math.random()).slice(0, 10);
@@ -36,7 +36,7 @@ export const getBookByYear = async (year) => {
   return fetch(
     `https://www.googleapis.com/books/v1/volumes?q=subject:fiction+publishedDate:${year}&orderBy=relevance&maxResults=40&key=${API_KEY}`
   )
-    .then(checServerResponce)
+    .then(checkServerResponce)
     .then((data) => {
       if (data.items) {
         return data.items.slice(0, 5);
